5a31bd101942dead2d59e2d8c00f11777a7c38fc
ishiy <y.ishihara.1993@gmail.com>
2017-07-01 05:23:55 +0900

    Add defDiffs

--- src/Encode.hs
+++ src/Encode.hs
@@ -1,9 +1,10 @@
 module Encode where
 
 import Data.Ord (comparing)
-import Data.List (intercalate, maximumBy)
+import Data.List (intercalate, maximumBy, sortBy)
 import qualified Data.Set as S
 import qualified Data.MultiSet as MS
+import Text.Printf
 
 import Format
 import Lib
@@ -14,6 +15,7 @@ toCode eom = unlines $
     [ "dimension :: " ++ show dim
     , "axes :: " ++ intercalate "," (map show axes)
     , ""
+    , defDiffs axes
     , encode eom
     , encode eomT
     ] ++ concatMap (\i -> map (encode . diffBy i) [eom, eomT]) axes
@@ -21,6 +23,89 @@ toCode eom = unlines $
         dim = length axes
         axes = S.toList $ S.delete T $ maximumBy (comparing S.size) $ map (dependOn . lhs) eom
         eomT = diffByT eom
+
+defDiffs :: [Coord] -> String
+defDiffs axes = unlines $ defDiff2 axes ++ defDiff3 axes
+
+defDiff :: [Coord] -> Coords -> String
+defDiff as ds = unwords [l, "=", r]
+    where l = "d" ++ formatDiff ds
+          r = "fun" ++ paren ("a" : map (\i -> "a_" ++ show i) as) ++ " " ++ defDiffBody (length as) ds
+
+defDiffBody :: Int -> Coords -> String
+defDiffBody dim ds
+    | typeOf ds == (2,1) = dii dim $ head (MS.distinctElems ds)
+    | typeOf ds == (2,2) = dij dim $ MS.distinctElems ds
+    | typeOf ds == (3,1) = diii dim $ head (MS.distinctElems ds)
+    | typeOf ds == (3,2) = diij dim $ fromMS ds
+    | otherwise = dijk
+    where
+        typeOf ds = (MS.size ds, MS.distinctSize ds)
+        fromMS = map fst . sortBy (comparing (negate.snd)) . MS.toOccurList
+
+data Index = Pre | Zero | Succ
+
+instance Show Index where
+    show Pre = "-1"
+    show Zero = ""
+    show Succ = "+1"
+
+dii :: Int -> Coord -> String
+dii dim i = printf "(%s + %s - 2%s - h*(%s - %s)/4)/h/h"
+                   (a Succ) (a Pre) (a Zero) (ai Succ) (ai Pre)
+    where
+        index ix | dim == 1 = bracket ["i" ++ show ix]
+                 | dim == 2 && i == X = bracket ["i" ++ show ix, "j"]
+                 | dim == 2 && i == Y = bracket ["i", "j" ++ show ix]
+                 | dim == 3 && i == X = bracket ["i" ++ show ix, "j", "k"]
+                 | dim == 3 && i == Y = bracket ["i", "j" ++ show ix, "k"]
+                 | dim == 3 && i == Z = bracket ["i", "j", "k" ++ show ix]
+        a ix = "a" ++ index ix
+        ai ix = "a_" ++ show i ++ index ix
+
+dij :: Int -> [Coord] -> String
+dij dim [i,j] = printf "(%s + %s - %s - %s)/2/h/h - (%s - %s - %s + %s + %s - %s + %s - %s)/8/h"
+                       (a Succ Succ) (a Pre Pre) (a Succ Pre) (a Pre Succ)
+                       (ai Succ Succ) (ai Pre Pre) (ai Succ Pre) (ai Pre Succ)
+                       (aj Succ Succ) (aj Pre Pre) (aj Succ Pre) (aj Pre Succ)
+    where
+        index di dj | dim == 2 = bracket ["i" ++ show di, "j" ++ show dj]
+                    | dim == 3 && (i,j) == (X,Y) = bracket ["i" ++ show di, "j" ++ show dj, "k"]
+                    | dim == 3 && (i,j) == (X,Z) = bracket ["i" ++ show di, "j", "k" ++ show dj]
+                    | dim == 3 && (i,j) == (Y,Z) = bracket ["i", "j" ++ show di, "k" ++ show dj]
+        a di dj = "a" ++ index di dj
+        ai di dj = "a_" ++ show i ++ index di dj
+        aj di dj = "a_" ++ show j ++ index di dj
+
+diii :: Int -> Coord -> String
+diii dim i = printf "(%s + %s - 2%s)/h/h" (ai Succ) (ai Pre) (ai Zero)
+    where
+        index ix | dim == 1 = bracket ["i" ++ show ix]
+                 | dim == 2 && i == X = bracket ["i" ++ show ix, "j"]
+                 | dim == 2 && i == Y = bracket ["i", "j" ++ show ix]
+                 | dim == 3 && i == X = bracket ["i" ++ show ix, "j", "k"]
+                 | dim == 3 && i == Y = bracket ["i", "j" ++ show ix, "k"]
+                 | dim == 3 && i == Z = bracket ["i", "j", "k" ++ show ix]
+        ai ix = "a_" ++ show i ++ index ix
+
+diij :: Int -> [Coord] -> String
+diij dim [i,j] = printf "(%s + %s - 2%s)/h/h" (aj Succ) (aj Pre) (aj Zero)
+    where
+        index ix | dim == 2 && i == X = bracket ["i" ++ show ix, "j"]
+                 | dim == 2 && i == Y = bracket ["i", "j" ++ show ix]
+                 | dim == 3 && i == X = bracket ["i" ++ show ix, "j", "k"]
+                 | dim == 3 && i == Y = bracket ["i", "j" ++ show ix, "k"]
+                 | dim == 3 && i == Z = bracket ["i", "j", "k" ++ show ix]
+        aj ix = "a_" ++ show j ++ index ix
+
+dijk :: String
+dijk = "(a_z[i+1,j+1,k] + a_z[i-1,j-1,k] - a_z[i+1,j-1,k] - a_z[i-1,j+1,k])/4/h/h"
+
+defDiff2 :: [Coord] -> [String]
+defDiff2 axes = map (defDiff axes) [MS.fromList [i,j] | i <- axes, j <- filter (>=i) axes]
+
+defDiff3 :: [Coord] -> [String]
+defDiff3 axes = map (defDiff axes) [MS.fromList [i,j,k] | i <- axes, j <- filter (>=i) axes, k <- filter (>=j) axes]
 
 encode :: EOM -> String
 encode eom = unlines $ [header]
@@ -76,3 +161,6 @@ mkEq n as ds = unwords [l, "=", r]
 
 paren :: [String] -> String
 paren ss = "(" ++ intercalate "," ss ++ ")"
+
+bracket :: [String] -> String
+bracket ss = "[" ++ intercalate "," ss ++ "]"
